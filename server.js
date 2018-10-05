885e0de8ae222517fc1d50295710905c06dd983b
Naresh,Nagarajan <naresh_nagarajan@homedepot.com>
2018-10-05 16:22:08 -0400

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 const express = require('express')
 var path = require('path')
 const app = express()
-let rootDir = "/Users/Shared/workspace/inventory-core/build/reports/gatling"
+let rootDir = "/Users/Shared/workspace/web-client/e2e-puppeteer/output/"
 app.use('/listRootDir', (req, res) => {
     var files = fs.readdirSync(rootDir);
     var resultArray = [];
@@ -28,6 +28,44 @@ app.use('/getIndex/:indexDir/fetchFilesForDisplay', (req, res) => {
     res.sendFile(rootDir+ '/'+req.params.indexDir + '/index.html');
 });
 
+var dir =  process.cwd();
 
+app.get('/files/*', function(req, res) {
+    console.log("indexDir ", req.params.indexDir);
+    var currentDir =  dir;
+    var query = req.query.path || '';
+    if (query) currentDir = path.join(dir, query);
+    console.log("browsing ", currentDir);
+    fs.readdir(currentDir, function (err, files) {
+        if (err) {
+           throw err;
+         }
+         var data = [];
+         files
+         .forEach(function (file) {
+           try {
+                   //console.log("processingile);
+                   var isDirectory = fs.statSync(path.join(currentDir,file)).isDirectory();
+                   if (isDirectory) {
+                     data.push({ Name : file, IsDirectory: true, Path : path.join(query, file)  });
+                   }
+                //    } else {
+                //      var ext = path.extname(file);
+                //      if(program.exclude && _.contains(program.exclude, ext)) {
+                //        console.log("excluding file ", file);
+                //        return;
+                //      }       
+                //      data.push({ Name : file, Ext : ext, IsDirectory: false, Path : path.join(query, file) });
+                //    }
+   
+           } catch(e) {
+             console.log(e); 
+           }        
+           
+         });
+         //data = _.sortBy(data, function(f) { return f.Name });
+         res.json(data);
+     });
+   });
 
 app.listen(5000, () => console.log('Example app listening on port 5000!'))
